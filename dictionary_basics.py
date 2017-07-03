ad765aec5bfcf5276c4dcce2bcc85cea5f71c1ed
Alyssa Nickow <alyssa@nickow.com>
2017-07-03 15:12:54 -0500

    completed dictionary basics activity

---
+++ dictionary_basics.py
@@ -0,0 +1,3 @@
+alyssa_info = {'name':'Alyssa', 'age':'36', 'country of birth': 'USA', 'favorite language':'Python'}
+for key, value in alyssa_info.items():
+    print "My {} is {}".format(key, value)
