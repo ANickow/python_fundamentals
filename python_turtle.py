367e033223fe7e4261b1a2a1cc2db421e7873d02
Alyssa Nickow <alyssa@nickow.com>
2017-07-03 14:18:06 -0500

    completed turtle activity

---
+++ python_turtle.py
@@ -0,0 +1,26 @@
+# try this either as a .py file or in the python shell
+import turtle
+
+# Sample turtle drawing from learning deck
+# the distance we want the pointer to travel each time
+# DIST = 100
+# for x in range(0,6):
+#     print "x", x
+#     for y in range(1,5):
+#         print "y", y
+#         # turn the pointer 90 degrees to the right
+#         turtle.right(90)
+#         # advance according to set distance
+#         turtle.forward(DIST)
+#     # add to set distance
+#     DIST += 20
+
+# My turtle drawing (makes a letter A)
+turtle.left(45)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.left(180)
+turtle.forward(25)
+turtle.left(45)
+turtle.forward(140)
