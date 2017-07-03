43edda1d09fbd4f86334fc39fdff001bbacea5f1
Alyssa Nickow <alyssa@nickow.com>
2017-07-03 15:19:40 -0500

    completed names activity

---
+++ names.py
@@ -0,0 +1,32 @@
+# Part I
+students = [
+     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'first_name' : 'John', 'last_name' : 'Rosales'},
+     {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+     {'first_name' : 'KB', 'last_name' : 'Tonel'}
+]
+
+for name in students:
+    print name['first_name'], name['last_name']
+
+# Part II
+users = {
+ 'Students': [
+     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'first_name' : 'John', 'last_name' : 'Rosales'},
+     {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+     {'first_name' : 'KB', 'last_name' : 'Tonel'}
+  ],
+ 'Instructors': [
+     {'first_name' : 'Michael', 'last_name' : 'Choi'},
+     {'first_name' : 'Martin', 'last_name' : 'Puryear'}
+  ]
+ }
+
+for category in users:
+    print category
+    count = 1
+    for name in users[category]:
+        length = len(name['first_name']) + len(name['last_name'])
+        print str(count), "-", name['first_name'], name['last_name'], "-", str(length)
+        count += 1
